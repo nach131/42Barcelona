8dbdd9524ff8e14d35a2c8e3619a995fe18c78c7
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-03-31 18:08:44 +0200

    inicio c

---
+++ C_video/02 variables/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+  // formar de declarar varables
+  // int atoma;
+  // int _atoma;
+  // int Atoma;
+
+int a;
+int b;
+
+a = 42;
+b = 32;
+
+printf("Hola que tal va el %d o el %d es mejor\n", a ,b); 
+}
