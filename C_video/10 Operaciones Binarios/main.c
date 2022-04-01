61a30265421327e204794a8867ec9366d37b5c38
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-01 11:18:21 +0200

    9 ok

---
+++ C_video/10 Operaciones Binarios/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// ++ incrementa
+// -- decrementa
+int main() 
+
+{
+ int i;
+ 
+
+ i = 41;
+
+
+  printf("Valor i original: %d\n",i);
+  printf("Valor i incrementado: %d\n",++i);
+  printf("Valor i decrementar: %d\n",--i);
+  
+}
