30cfbca092fe078a49dd6afd6baa71a071912c9d
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-01 09:03:43 +0200

    toma

--- C_video/04 Tipos de variables/main.c
+++ C_video/04 Tipos de variables/main.c
@@ -2,12 +2,34 @@
 int main() 
 {
 
+// solo para char o int
+// unsigned = 0 -> 255
+// signed = -128 -> 127
+
+// signed carh c;
+// unsigned int i;
+
+// long int ==> vale 8
+// short
 // char la varible mas pequeña contiene un octeto
 char c;
 c ='k';
+
+// enteros
+int i;
+
+//===========================================================================================
+
+// numeros con decimales de lag "Flotante" 4 octetos
+float f;
+f= 42.12;
+
+// igual que float pero del doble de tamaño 8 octetos
+double d;
+
 //SABER EL VALOR DE UNA VARIABLE sizeof(variable)
-printf("%lu %d %c\n",sizeof(c),c,c); // devuelve 1 = un octeto = 127 bits, 107 el valor , k caracter
-
+printf("Tipos de variables:\n"); // devuelve 1 = un octeto = 127 bits, 107 el valor , k caracter
+printf("char: %lu %d %c\n",sizeof(c),c,c); // devuelve 1 = un octeto = 127 bits, 107 el valor , k caracter
+printf("int: %lu\n",sizeof(i)); //
+printf("float: %lu %f\n",sizeof(f),f); //
 }
-
-// %lu = 1
---
+++ C_video/05 Scopes/README.md
@@ -0,0 +1,13 @@
+# 42Barcelona
+
+Ambito o alzance de una variable
+
+Ambito es lo que circula entre dos llaves {}
+
+Ambito global es lo que esta a fuera de toda función. 
+
+ejemplo función:
+  int manin()
+  {
+    
+  }
