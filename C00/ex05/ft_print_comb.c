6edbf752a6a767b3eb5bfc3f5162ae76b7f9665b
nacho <nacho.mota@gmail.com>
2022-04-04 22:09:44 +0200

    actualizado ft_print_comb y borrado dos.c

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,31 +1,35 @@
 #include <unistd.h>
-
-void	ft_print_comb(void)
+void ft_putchar(char c, char d,char u)
 {
-	char	c;
-	char	d;
-	char	u;
+write(1, &c, 1);
+write(1, &d, 1);
+write(1, &u, 1);
 
-	c = '0';
-	while	(c <= '7')
-	{
-		d = c + 1;
-		while ( d <= '8')
-		{	
-			u = c + 1;
-			while(c <= '9')
+}
+
+void ft_print_comb(void)
+{
+char centenas;
+char decenas;
+char unidades;
+
+	centenas = '0';
+	while (centenas <= '7')
+	{	
+	decenas = centenas + 1;
+		while(decenas <= '8')
+		{
+		unidades = decenas +1;
+			while(unidades <= '9')
 			{
-			write(1, &c, 1);	
-			write(1, &d, 1);
-			write(1, &u, 1);
-			if	(!(c == '7' && d == '8' && u == '9'))
-			write(1, ", ",2);
-			u++;
+			ft_putchar(centenas, decenas, unidades);
+			write(1, ", ", 2);
+			unidades++;
 			}
-		d++;
+		decenas++;
 		}
-	c++;
-	}	
+	centenas++;
+	}
 
 
 }
