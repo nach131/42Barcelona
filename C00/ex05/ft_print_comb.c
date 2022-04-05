3057c4e1782942fd18aaaa203fe4d52cbbd2986a
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-05 16:28:07 +0200

    upgrade ex6

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,35 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 12:10:29 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/04/05 10:07:51 by nmota-bu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*Ascii 0=48 9=57*/
 #include <unistd.h>
-void ft_putchar(char c, char d,char u)
-{
-write(1, &c, 1);
-write(1, &d, 1);
-write(1, &u, 1);
 
+void	ft_putchar(char c, char d, char u)
+{
+	write(1, &c, 1);
+	write(1, &d, 1);
+	write(1, &u, 1);
 }
 
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
-char centenas;
-char decenas;
-char unidades;
+	char	centenas;
+	char	decenas;
+	char	unidades;
 
 	centenas = '0';
 	while (centenas <= '7')
-	{	
-	decenas = centenas + 1;
-		while(decenas <= '8')
+	{
+		decenas = centenas +1;
+		while (decenas <= '8')
 		{
-		unidades = decenas +1;
-			while(unidades <= '9')
+			unidades = decenas +1;
+			while (unidades <= '9')
 			{
-			ft_putchar(centenas, decenas, unidades);
-			write(1, ", ", 2);
-			unidades++;
+				ft_putchar(centenas, decenas, unidades);
+				unidades++;
+				write(1, " ,", 2);
 			}
-		decenas++;
+			decenas++;
 		}
-	centenas++;
+		centenas++;
 	}
-
-
 }
