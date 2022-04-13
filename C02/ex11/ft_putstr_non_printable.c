18b9dcec35a83555fce1939412f232b63948595c
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-13 11:04:13 +0200

    toma....ls

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 21:52:29 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/04/12 21:52:34 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/04/13 11:03:24 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,15 @@ void	ft_putstr_non_printable(char *str)
 	hexadecimal = "0123456789abcdef";
 	while (*str != '\0')
 	{
-		if (*str >= 32 && *str <= 126)
-			ft_putchar(*str);
-		else
+		if (*str <= 31 && *str >= 0 || *str == 127)
 		{
 			ft_putchar('\\');
 			ft_putchar(hexadecimal[*str / 16]);
 			ft_putchar(hexadecimal[*str % 16]);
+		}
+		else
+		{
+			ft_putchar(*str);
 		}
 		str++;
 	}
