a2605124fe47d7cf2b79dc1ebf8aea6d6cce7e7b
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-14 15:00:04 +0200

    cosita

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 11:51:53 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/04/13 15:35:39 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/04/14 14:54:39 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@
 
 //  strcmp — Comparación de string segura a nivel binario
 //  Tenga en cuenta que esta comparación considera mayúsculas y minúsculas.
-//  mientras (caracter s1 "Y" Caracter s2) "Y" (caracter s1 "IGUAL" Caracter s2) 
+//  mientras (caracter s1 "Y" Caracter s2) "Y" (caracter s1 "IGUAL"
+//  Caracter s2) 
 //  while ((s1[indice] && s2[indice]) && (s1[indice] == s2[indice]))
 //  se devuelve 
 
-
 int	ft_strcmp(char *s1, char *s2)
 {
-	int indice;
-	int toma;
+	int	indice;
+	int	toma;
 
 	toma = 42;
 	while ((s1[indice] && s2[indice]) && (s1[indice] == s2[indice]))
--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,9 +6,19 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 12:45:49 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/04/14 13:31:59 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/04/14 14:57:13 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/* while mientras caracter sea menor que "n", ADN diferencia de "diferente"
+ * AND la posicion s1[caracter] no sea null '\0' AND s2[caracter] no se
+ * null '\0' 
+ *asgignar a diferencia s1[caracter] - s2[caracter]
+ *
+ *SI caracter menor n AND diferente a diferencia AND s1[caracter] es null
+ * OR s2[caracter] igual a null 
+ *
+ * */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
@@ -20,7 +30,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	while ((caracter < n) && !diferencia && (s1[caracter] != '\0')
 		&& (s2[caracter] != '\0'))
 	{
-		diferencia = (unsigned char)s1[caracter] - (unsigned char)s2[caracter];
+		diferencia = (unsigned char)s1[caracter] - (unsigned char)s2[:caracter];
 		caracter++;
 	}
 	if (caracter < n && !diferencia && (s1[caracter] == '\0'
