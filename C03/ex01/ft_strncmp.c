2d6b3495d8fd7065b6c6ae2e0bcd23fc6235f27d
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-14 13:35:42 +0200

    ex01

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,26 +6,25 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 12:45:49 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/04/14 13:10:39 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/04/14 13:31:59 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int caracter;
-	int diferencia;
+	unsigned int	caracter;
+	int				diferencia;
 
 	caracter = 0;
 	diferencia = 0;
-	
-	while((caracter < n) && !diferencia && (s1[caracter] != '\0')
-			&& (s2[caracter] = '\0'))	
+	while ((caracter < n) && !diferencia && (s1[caracter] != '\0')
+		&& (s2[caracter] != '\0'))
 	{
 		diferencia = (unsigned char)s1[caracter] - (unsigned char)s2[caracter];
 		caracter++;
 	}
 	if (caracter < n && !diferencia && (s1[caracter] == '\0'
-				|| s2[caracter] == '\0'))
+			|| s2[caracter] == '\0'))
 		diferencia = (unsigned char)s1[caracter] - (unsigned char)s2[caracter];
 	return (diferencia);
 }
