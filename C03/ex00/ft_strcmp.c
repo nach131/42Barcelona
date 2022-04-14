fc4a7871f2fd8430957d5dd004acba5e7875b299
nacho <nacho.mota@gmail.com>
2022-04-14 21:50:43 +0200

    ex00 okk

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -5,32 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/13 11:51:53 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/04/14 15:12:32 by nmota-bu         ###   ########.fr       */
+/*   Created: 2022/04/14 21:29:43 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/04/14 21:29:43 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-
-// char es una variable con signo, que tiene un rango de [-128,127].
-// unsigned char es una variable sin signo, que tiene un rango de [0,255]
-
-//  strcmp — Comparación de string segura a nivel binario
-//  Tenga en cuenta que esta comparación considera mayúsculas y minúsculas.
-//  mientras (caracter s1 "Y" Caracter s2) "Y" (caracter s1 "IGUAL"
-//  Caracter s2) 
-//  while ((s1[indice] && s2[indice]) && (s1[indice] == s2[indice]))
-//  se devuelve 
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	indice;
-	int	toma;
 
-	toma = 42;
-	while ((s1[indice] && s2[indice]) && (s1[indice] == s2[indice]))
+	indice = 0;
+	while (s1[indice] != '\0' || s2[indice] != '\0')
 	{
+		if (s1[indice] != s2[indice])
+		{
+			return (s1[indice] - s2[indice]);
+		}
 		indice++;
 	}
-	return ((unsigned char)s1[indice] - (unsigned char)s2[indice]);
+	return (0);
 }
