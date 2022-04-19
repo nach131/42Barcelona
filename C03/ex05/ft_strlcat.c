20cbaa9fadcdd94076f0508fd59514be8425796f
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-19 16:59:49 +0200

    ..

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,9 +6,11 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 14:39:20 by yeselee           #+#    #+#             */
-/*   Updated: 2022/04/17 16:41:16 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/04/19 16:45:13 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include<stdio.h>
 
 unsigned int	ft_strlen(char *str)
 {
@@ -41,4 +43,16 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	}
 	dest[dest_len + indice] = '\0';
 	return (dest_len + src_len);
+}
+
+int	main(void)
+{
+	char *s1_test1_o = "a";
+	char s2_test1_o[100] = "b";
+	unsigned int n;
+	unsigned int len_obt;
+	n = 10;
+	len_obt = ft_strlcat(s2_test1_o, s1_test1_o, n);
+
+	printf("resultado: %d\n", len_obt);
 }
