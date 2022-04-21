11346085475421a759593f0bb497c666ccc531e5
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-04-21 11:10:46 +0200

    norminete ok

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 20:37:13 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/04/19 20:37:20 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/04/21 11:09:06 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,32 @@
 /* ║                 https://github.com/nach131/42Barcelona                 ║ */
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
-int ft_is_prime(long long nb)
+int	ft_is_prime(long long nb)
 {
-        long long i;
+	long long	i;
 
-        i = 2;
-        if (nb <= 1)
-                return (0);
-        while (i * i <= nb)
-        {
-                if (nb % i == 0)
-                        return (0);
-                i++;
-        }
-        return (1);
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int ft_find_next_prime(int nb)
+int	ft_find_next_prime(int nb)
 {
-        long long i;
+	long long	i;
 
-        i = (long long)nb;
-        while (i <= 2147483647)
-        {
-                if (ft_is_prime(i) == 1)
-                        return ((int)i);
-                i++;
-        }
-        return (0);
+	i = (long long)nb;
+	while (i <= 2147483647)
+	{
+		if (ft_is_prime(i) == 1)
+			return ((int)i);
+		i++;
+	}
+	return (0);
 }
