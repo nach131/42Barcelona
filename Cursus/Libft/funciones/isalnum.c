ddcc27668084e61713221ddec87a10e8c89ae35b
nacho <nacho.mota@gmail.com>
2022-05-12 13:43:23 +0200

    funciones originales

--- Cursus/Libft/funciones/isalnum.c
+++ Cursus/Libft/funciones/isalnum.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   isalnum.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/11 23:06:19 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/05/12 09:58:37 by nmota-bu         ###   ########.fr       */
+/*   Created: 2022/05/12 13:40:36 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/05/12 13:42:48 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,16 @@
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
 #include <stdio.h>
-//ascii 48-0 to 57-9
+#include <ctype.h>
 
-int	ft_isdigit(int c)
-{
-	if(c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
+int	main(){
+	int i;
+	i = 'W';
+	printf("Resultado isdigit de %c: %d\n",i, isalnum(i));
+	i = '5';
+	printf("Resultado isdigit de %c: %d\n",i, isalnum(i));
+	i = '@';
+	printf("Resultado isdigit de %c: %d\n",i, isalnum(i));
+
 }
 
-int main(void)
-{
-	char n;
-	printf("Escribe un caracter para comprobar si es digito:\n");
-	scanf("%c", &n);
-	printf("Resultado de '%c' es: %d\n",n,ft_isdigit(n));;
-}
--- Cursus/Libft/src_main/ft_isalnum.c
+++ Cursus/Libft/src_main/ft_isalnum.c
@@ -1,34 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/11 23:06:19 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/05/12 09:58:37 by nmota-bu         ###   ########.fr       */
+/*   Created: 2022/05/12 09:35:41 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/05/12 10:03:56 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* ╔════════════════════════════════════════════════════════════════════════╗ */
 /* ║                 https://github.com/nach131/42Barcelona                 ║ */
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
+//Ascii 48-0 to 57-9, 65-A to 90-A, 97-a to 122-z
 
-#include <stdio.h>
-//ascii 48-0 to 57-9
+#include<stdio.h>
 
-int	ft_isdigit(int c)
+int	ft_isalnum(int c)
 {
-	if(c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
+	if(c )
+
+return (1);
 }
 
 int main(void)
 {
 	char n;
-	printf("Escribe un caracter para comprobar si es digito:\n");
+	printf("Escribe un caracte para combrobar si es Alfanumerico:\n");
 	scanf("%c", &n);
-	printf("Resultado de '%c' es: %d\n",n,ft_isdigit(n));;
+	printf("El resultado '%c' es: %d\n", n, ft_isalnum(n));;
 }
