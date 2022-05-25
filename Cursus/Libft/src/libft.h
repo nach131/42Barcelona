d0055ed875056eb82709234b30d7d64c2eebf14b
nacho <nacho.mota@gmail.com>
2022-05-25 23:23:06 +0200

    ft_memcmp.c

--- Cursus/Libft/src/libft.h
+++ Cursus/Libft/src/libft.h
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 15:07:40 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/05/24 21:38:46 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/05/25 21:49:12 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
+int		ft_memcmp(const void *str1, const void *str2, size_t n);
 
 #endif
---
+++ Cursus/Libft/src_main/ft_memcmp.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/25 21:01:34 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/05/25 23:15:03 by nmota-bu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ╔════════════════════════════════════════════════════════════════════════╗ */
+/* ║                 https://github.com/nach131/42Barcelona                 ║ */
+/* ╚════════════════════════════════════════════════════════════════════════╝ */
+
+#include<stdio.h>
+#include"../src/libft.h"
+
+int	ft_memcmp(const void *str1, const void *str2, size_t n)
+{
+    const unsigned char *s1;
+    const unsigned char *s2;
+    size_t  i;
+
+    s1 = str1;
+    s2 = str2;
+    i = 0;
+    while (i < n)
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+            i++;
+    }
+    return (0);
+}
+//{
+//  size_t  i
+//  i = 0;
+//  while (i < n )
+//  {
+//  if (*(unsigned char *)(str1 + i ) != *(unsigned char *)(str2 + i))
+//      return (*(unsigned char *)(str1 + i) - *(unsigned char *)(str2 + i));
+//  i++;
+//  }
+//  return(0);
+//
+}
+int	main(void)
+{
+	int	res;
+	char str1[] = "AbCD";
+	char str2[] = "ABcd";
+
+	res = ft_memcmp(str1, str2, 2);
+
+	printf("res: %d\n", res);
+
+}
