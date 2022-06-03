337e7445238d7cda08615193e8ae4b1f5eb1907b
Nacho Mota buscetto <nmota-bu@student.42barcelona.com>
2022-06-03 13:35:34 +0200

    ft_substr

--- Cursus/Libft/src/ft_substr.c
+++ Cursus/Libft/src/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 10:19:29 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/06/02 12:48:37 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/06/03 13:34:04 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,54 +17,18 @@
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
-//{
-//	char 	*res;	
-//	size_t	len_s;
-//
-//	len_s = ft_strlen(s);
-////	res = (char *)s;
-//	if (len > (len_s - start))
-//		len = len_s - start;
-//	else if (start > len_s)
-//		len = 0;
-//	res = (char *)malloc(sizeof(*s) * (len + 1));
-//	if (!s)
-//		return (NULL);
-//	ft_strlcpy(res, &((char *)s)[start], (len + 1));
-//	printf(" string: %s, len:%zu\n start: %d \n len: %zu\n", s, len_s, start, len);
-//	printf("res: %s\n",res);
-//	return (res);
-//}
 {
-	char 	*res;	
+	char	*res;
 	size_t	len_s;
 
 	len_s = ft_strlen(s);
 	if (len > (len_s - start))
 		len = len_s - start;
-	else if (start > len_s)
-		len = 0;
+	if (start > len_s)
+		return (ft_strdup(""));
 	res = (char *)malloc(sizeof(*s) * (len + 1));
-	if (!s)
+	if (!res)
 		return (NULL);
-	ft_strlcpy(res, &((char *)s)[start], (len + 1));
+	strlcpy(res, &((char *)s)[start], (len + 1));
 	return (res);
 }
-
-//int	main(void)
-//{
-//	char	str[] = "42 Barcelona";
-//	int		start;
-//	int		len;
-//	char	*res;
-//
-//	int	x;
-//	x = sizeof(res);
-//
-//	start = 3;
-//	len	=	9;
-//
-////	res = ft_substr(NULL, start, len);
-//	res = ft_substr(str, start, len);
-//	printf("\nres: %s\n len_res: %i\n", res, x);
-//}
--- Cursus/Libft/src_main/ft_substr.c
+++ Cursus/Libft/src_main/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 10:19:29 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/06/02 12:46:05 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/06/03 13:33:03 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,36 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char 	*res;	
+	char	*res;
 	size_t	len_s;
 
 	len_s = ft_strlen(s);
-//	res = (char *)s;
 	if (len > (len_s - start))
 		len = len_s - start;
-	else if (start > len_s)
-		len = 0;
+	if (start > len_s)
+		return (ft_strdup(""));
 	res = (char *)malloc(sizeof(*s) * (len + 1));
-	if (!s)
+	if (!res)
 		return (NULL);
-	ft_strlcpy(res, &((char *)s)[start], (len + 1));
-	printf(" string: %s, len:%zu\n start: %d \n len: %zu\n", s, len_s, start, len);
-	printf("res: %s\n",res);
+	strlcpy(res, &((char *)s)[start], (len + 1));
 	return (res);
 }
 
-int	main(void)
-{
-	char	str[] = "42 Barcelona";
-	int		start;
-	int		len;
-	char	*res;
-
-	int	x;
-	x = sizeof(res);
-
-	start = 3;
-	len	=	9;
-
-//	res = ft_substr(NULL, start, len);
-	res = ft_substr(str, start, len);
-	printf("\nres: %s\n len_res: %i\n", res, x);
-}
+//int	main(void)
+//{
+////	char	str[] = "42 Barcelona";
+//	char	str[] = "";
+//	int		start;
+//	int		len;
+//	char	*res;
+//
+//	int	x;
+//	x = sizeof(res);
+//	
+//	start = 0;
+//	len	=	42000;
+//
+////	res = ft_substr(NULL, start, len);
+//	res = ft_substr(str, start, len);
+//	printf("\nres: %s\n len_res: %i\n", res, x);
+//}
