6a9c4724865b59a4ec5dc58b16d48e70e992328a
nacho <nacho.mota@gmail.com>
2022-06-05 09:31:40 +0200

    working

--- Cursus/Libft/src/ft_strtrim.c
+++ Cursus/Libft/src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 15:02:58 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/06/03 21:02:02 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/06/03 21:33:17 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,37 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!*s1 || !*set)
 		return (NULL);
 	i = 0;
-// por delante
-	while (ft_strchr((char *)set, s1[i]) != NULL && s1[i] != '\0')
+// recorta por delante
+	while (ft_strchr((char *)set, s1[i]) != NULL &&
+			s1[i] != '\0')
 	{
 		printf("%c,",s1[i]);
 		i++;
 	}
-// por detras
-len = ft_strlen(&s1[i]);
-	return ((char *)s1);
+// recorta por detras
+	len = ft_strlen(&s1[i]);
+	while (ft_strchr((char *)set, s1[len + i - 1]) != NULL &&
+			s1[i + len - 1] != '\0')
+		{	
+		printf("%c,",s1[len]);
+		len--;
+		}
+	return (ft_substr(s1, i, len));
 	}
 int	main(void)
 {
-
-	printf("\nOriginal: 42 Barcelona\nres:  %s\n", ft_strtrim("42 Barcelona", "24"));
-
+	char	str[] = "3123267542Barcelona90809878";
+	char	trim[] ="123567890";
+	char	*res;
+	
+	res = ft_strtrim(str, trim);
+	printf("\n\nOriginal: %s\nTrim: %s\nres: %s\n",str, trim, res);
+//	printf("\nOriginal: 42 Barcelona\nres:  %s\n", ft_strtrim("42 Barcelona 42", "24"));
 }
+//ft_res: 123567890
+//
+//3,1,2,3,2,6,7,5,9,a,n,o,l,e,c,r,
+//
+//Original: 3123267542Barcelona90809878
+//Trim: 123567890
+//res: 42Barcelona
