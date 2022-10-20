9558bf2bc8ff3a2c1e074f3073eed0908de3a5f9
nacho <nacho.mota@gmail.com>
2022-10-20 23:17:01 +0200

    mask..

--- C/bits/mascaras/README.md
+++ C/bits/mascaras/README.md
@@ -25,8 +25,28 @@ operacion(amarillo | azul | verder | blanco)
 3	blanco		0 0 0 0 1 0 0 0
 -------------------------------------------
 			1 0 1 1 1 0 0 0
-<<<<<<< HEAD
 ```
-=======
+```c
+#include <stdio.h>
+
+#define AMARILLO 1 << 7
+#define ROJO 1 << 6
+#define AZUL 1 << 5
+#define VERDE 1 << 4
+#define BLANCO 1 << 3
+#define NEGRO 1 << 2
+#define MAGENTA 1 << 1
+#define CIAN 1 << 0
+
+void operacion(unsigned char colors)
+{
+	printf("colors = %u", colors); // colors = 184 - 1 0 1 1  1 0 0 0
+}
+
+int main()
+{
+	operacion(AMARILLO | AZUL | VERDE | BLANCO);
+}
 ```
->>>>>>> 39f6779c33dad6823815fb0cb2ca80f0e7f03724
+
+Para la comprobación
--- C/bits/mascaras/mask.c
+++ C/bits/mascaras/mask.c
@@ -6,10 +6,31 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 21:41:40 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/10/20 22:05:28 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/10/20 23:16:29 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* ╔════════════════════════════════════════════════════════════════════════╗ */
 /* ║                 https://github.com/nach131/42Barcelona                 ║ */
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
+
+#include <stdio.h>
+
+#define AMARILLO 1 << 7
+#define ROJO 1 << 6
+#define AZUL 1 << 5
+#define VERDE 1 << 4
+#define BLANCO 1 << 3
+#define NEGRO 1 << 2
+#define MAGENTA 1 << 1
+#define CIAN 1 << 0
+
+void operacion(unsigned char colors)
+{
+	printf("colors = %u", colors); // colors = 184 - 1 0 1 1  1 0 0 0
+}
+
+int main()
+{
+	operacion(AMARILLO | AZUL | VERDE | BLANCO);
+}
