112a69aff8a4379816c7b277dfe4d7434ec99e58
nacho <nacho.mota@gmail.com>
2022-11-25 09:24:10 +0100

    colors & spinner

--- C/colores/code_colors.c
+++ C/colores/code_colors.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 22:56:19 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/11/07 23:14:53 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/11/08 09:32:36 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,73 +20,73 @@
 int main(void)
 {
 
-	printf("=== Regular Colors === \n");
-	printf(Black "Black\n");
-	printf(Red "Red\n");
-	printf(Green "Green\n");
-	printf(Yellow "Yellow\n");
-	printf(Blue "Blue\n");
-	printf(Magenta "Magenta\n");
-	printf(Cyan "Cyan\n");
-	printf(White "White\n");
+	printf("=== Regular Colors === \n" RESET);
+	printf(Black "Black\n" RESET);
+	printf(Red "Red\n" RESET);
+	printf(Green "Green\n" RESET);
+	printf(Yellow "Yellow\n" RESET);
+	printf(Blue "Blue\n" RESET);
+	printf(Magenta "Magenta\n" RESET);
+	printf(Cyan "Cyan\n" RESET);
+	printf(White "White\n" RESET);
 
-	printf("=== Bold === \n");
-	printf(BBlack "BBlack\n");
-	printf(BRed "BRed\n");
-	printf(BGreen "BGreen\n");
-	printf(BYellow "BYellow\n");
-	printf(BBlue "BBlue\n");
-	printf(BMagenta "BMagenta\n");
-	printf(BCyan "BCyan\n");
-	printf(BWhite "BWhite\n");
+	printf("=== Bold === \n" RESET);
+	printf(BBlack "BBlack\n" RESET);
+	printf(BRed "BRed\n" RESET);
+	printf(BGreen "BGreen\n" RESET);
+	printf(BYellow "BYellow\n" RESET);
+	printf(BBlue "BBlue\n" RESET);
+	printf(BMagenta "BMagenta\n" RESET);
+	printf(BCyan "BCyan\n" RESET);
+	printf(BWhite "BWhite\n" RESET);
 
-	printf("=== Underline === \n");
-	printf(UBlack "UBlack\n");
-	printf(URed "URed\n");
-	printf(UGreen "UGreen\n");
-	printf(UYellow "UYellow\n");
-	printf(UBlue "UBlue\n");
-	printf(UMagenta "UMagenta\n");
-	printf(UCyan "UCyan\n");
-	printf(UWhite "UWhite\n");
+	printf("=== Underline === \n" RESET);
+	printf(UBlack "UBlack\n" RESET);
+	printf(URed "URed\n" RESET);
+	printf(UGreen "UGreen\n" RESET);
+	printf(UYellow "UYellow\n" RESET);
+	printf(UBlue "UBlue\n" RESET);
+	printf(UMagenta "UMagenta\n" RESET);
+	printf(UCyan "UCyan\n" RESET);
+	printf(UWhite "UWhite\n" RESET);
 
-	// printf("=== Background === \n");
-	// printf(On_Black "On_Black\n");
-	// printf(On_Red "On_Red\n");
-	// printf(On_Green "On_Green\n");
-	// printf(On_Yellow "On_Yellow\n");
-	// printf(On_Blue "On_Blue\n");
-	// printf(On_Magenta "On_Magenta\n");
-	// printf(On_Cyan "On_Cyan\n");
-	// printf(On_White "On_White\n");
+	// printf("=== Background === \n" RESET);
+	// printf(On_Black "On_Black\n" RESET);
+	// printf(On_Red "On_Red\n" RESET);
+	// printf(On_Green "On_Green\n" RESET);
+	// printf(On_Yellow "On_Yellow\n" RESET);
+	// printf(On_Blue "On_Blue\n" RESET);
+	// printf(On_Magenta "On_Magenta\n" RESET);
+	// printf(On_Cyan "On_Cyan\n" RESET);
+	// printf(On_White "On_White\n" RESET);
 
-	printf("=== High Intensity === \n");
-	printf(IBlack "IBlack\n");
-	printf(IRed "IRed\n");
-	printf(IGreen "IGreen\n");
-	printf(IYellow "IYellow\n");
-	printf(IBlue "IBlue\n");
-	printf(IMagenta "IMagenta\n");
-	printf(ICyan "ICyan\n");
-	printf(IWhite "IWhite\n");
+	printf("=== High Intensity === \n" RESET);
+	printf(IBlack "IBlack\n" RESET);
+	printf(IRed "IRed\n" RESET);
+	printf(IGreen "IGreen\n" RESET);
+	printf(IYellow "IYellow\n" RESET);
+	printf(IBlue "IBlue\n" RESET);
+	printf(IMagenta "IMagenta\n" RESET);
+	printf(ICyan "ICyan\n" RESET);
+	printf(IWhite "IWhite\n" RESET);
 
-	printf("=== Bold High Intensity === \n");
-	printf(BIBlack "BIBlack\n");
-	printf(BIRed "BIRed\n");
-	printf(BIGreen "BIGreen\n");
-	printf(BIYellow "BIYellow\n");
-	printf(BIBlue "BIBlue\n");
-	printf(BIMagenta "BIMagenta\n");
-	printf(BICyan "BICyan\n");
-	printf(BIWhite "BIWhite\n");
+	printf("=== Bold High Intensity === \n" RESET);
+	printf(BIBlack "BIBlack\n" RESET);
+	printf(BIRed "BIRed\n" RESET);
+	printf(BIGreen "BIGreen\n" RESET);
+	printf(BIYellow "BIYellow\n" RESET);
+	printf(BIBlue "BIBlue\n" RESET);
+	printf(BIMagenta "BIMagenta\n" RESET);
+	printf(BICyan "BICyan\n" RESET);
+	printf(BIWhite "BIWhite\n" RESET);
 
-	// printf("=== High Intensity backgrounds=== \n");
-	// printf(On_Black "On_Black\n");
-	// printf(On_Red "On_Red\n");
-	// printf(On_Green "On_Green\n");
-	// printf(On_Yellow "On_Yellow\n");
-	// printf(On_Blue "On_Blue\n");
-	// printf(On_Magenta "On_Magenta\n");
-	// printf(On_Cyan "On_Cyan\n");
-	// printf(On_White "On_White\n");
+	// printf("=== High Intensity backgrounds=== \n" RESET);
+	// printf(On_Black "On_Black\n" RESET);
+	// printf(On_Red "On_Red\n" RESET);
+	// printf(On_Green "On_Green\n" RESET);
+	// printf(On_Yellow "On_Yellow\n" RESET);
+	// printf(On_Blue "On_Blue\n" RESET);
+	// printf(On_Magenta "On_Magenta\n" RESET);
+	// printf(On_Cyan "On_Cyan\n" RESET);
+	// printf(On_White "On_White\n" RESET);
 }
--- C/colores/colore.c
+++ C/colores/colore.c
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 22:02:39 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/06/17 22:10:07 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/11/18 19:11:39 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,49 @@
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
 #include <stdio.h>
+#include <unistd.h>
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define NNRM "\x1B[0m"
+#define NRED "\x1B[31m"
+#define NGRN "\x1B[32m"
+#define NYEL "\x1B[33m"
+#define NBLU "\x1B[34m"
+#define NMAG "\x1B[35m"
+#define NCYN "\x1B[36m"
+#define NWHT "\x1B[37m"
+
+#define BNRM "\x1B[40m"
+#define BRED "\x1B[41m"
+#define BGRN "\x1B[42m"
+#define BYEL "\x1B[43m"
+#define BBLU "\x1B[44m"
+#define BMAG "\x1B[45m"
+#define BCYN "\x1B[46m"
+#define BWHT "\x1B[47m"
 
 int main()
 {
-    printf("%sred\n", KRED);
-    printf("%sgreen\n", KGRN);
-    printf("%syellow\n", KYEL);
-    printf("%sblue\n", KBLU);
-    printf("%smagenta\n", KMAG);
-    printf("%scyan\n", KCYN);
-    printf("%swhite\n", KWHT);
-    printf("%snormal\n", KNRM);
+	write(1, NNRM "BLACK\n", 11);
+	write(1, NRED "RED\n", 9);
+	write(1, NGRN "GREEN\n", 12);
+	write(1, NYEL "YELLOW\n", 13);
+	write(1, NBLU "BLUE\n", 13);
+	write(1, NMAG "MAGENTA\n", 15);
+	write(1, NCYN "CYAN\n", 10);
+	write(1, NWHT "WHITE\n", 12);
 
-    return 0;
+	write(1, NNRM "==========\n", 16);
+	write(1, NNRM "BLACK\n", 15);
+	write(1, BWHT NRED "RED\n", 9);
+	write(1, NGRN "GREEN\n", 12);
+	write(1, NYEL "YELLOW\n", 13);
+	write(1, NBLU "BLUE\n", 13);
+	write(1, NMAG "MAGENTA\n", 15);
+	write(1, NCYN "CYAN\n", 10);
+	write(1, NWHT "WHITE\n", 12);
+
+	return 0;
 }
+
+// BRED NYEL
+// BGRN NBLU
